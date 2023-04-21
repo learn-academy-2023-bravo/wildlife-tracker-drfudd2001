d6e76ede1c2b1e53cc861f45b5da285a797d4271
drfudd2001 <elmer.figueroagarcia@gmail.com>
2023-04-21 10:03:40 -0700

    "Completed stories 1-3"

---
+++ db/migrate/20230420230727_create_sightings.rb
@@ -0,0 +1,11 @@
+class CreateSightings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sightings do |t|
+      t.float :longitude
+      t.float :latitude
+      t.date :date
+      t.integer :animal_id
+      t.timestamps
+    end
+  end
+end
